d95e4243299425b8571bf3763f4564ff4587cd38
Brent Michalski <brent@brents-imac.local>
2017-09-07 20:36:19 -0500

    new resources

--- Turd Herder/GameScene.swift
+++ Turd Herder/GameScene.swift
@@ -14,7 +14,7 @@ import GameplayKit
 class GameScene: SKScene {
     var targetCreationRate:TimeInterval = 0.25
     
-    let maxTargets       = 20
+    let maxTargets       = 2
     var currentFartIndex = 0
     
     var hud = HUD()
